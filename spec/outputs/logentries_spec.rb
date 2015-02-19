39193e37640bf90c073fe05274f92b86dc475d22
Bart <siniar1990@gmail.com>
2015-02-19 21:27:11 +0000

    Minor Changes

--- spec/outputs/logentries_spec.rb
+++ spec/outputs/logentries_spec.rb
@@ -1,3 +1,3 @@
 require "logstash/devutils/rspec/spec_helper"
 
-gem ‘logstash-devutils’, :git => 'https://github.com/elasticsearch/logstash-devutils'
+gem ‘logstash-devutils’, :git => 'git://github.com/elasticsearch/logstash-devutils.git'
